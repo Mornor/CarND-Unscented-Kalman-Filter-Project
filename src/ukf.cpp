41400f8a5637f7b90e87cf3056fbeb146bbadc2d
Celien Nanson <cesliens@gmail.com>
2017-03-15 21:50:32 +0100

    Add the prediciton step

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -5,6 +5,9 @@
  * Initializes Unscented Kalman filter
  */
 UKF::UKF() {
+
+	is_initialized_ = false;
+
 	// if this is false, laser measurements will be ignored (except during init)
 	use_laser_ = true;
 
@@ -38,7 +41,8 @@ UKF::UKF() {
 	// Radar measurement noise standard deviation radius change in m/s
 	std_radrd_ = 0.3;
 
-	// Set state dimension
+	// Set state dimension (px, py, speed v (magnitude of the velocty), Si (angle of the orientation toward which the tracking object move
+	// and yaw rate Si dot)
 	int n_x = 5;
 
 	// Set augmented dimension
@@ -58,6 +62,95 @@ UKF::~UKF() {}
  * either radar or laser.
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
+
+	/*****************************************************************************
+	*  Initialization
+	****************************************************************************/
+
+	if(!is_initialized_){
+
+		 // first measurement, we do not now px and py (position x and y), neither vx and vy
+        float p_x = 0; 
+        float p_y = 0;
+        float v = 0;  // Speed, magnitude of the velocity
+        float yaw_rate = 0; 
+        float yaw_rate_dot = 0; 
+
+        if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+            float rho = measurement_pack.raw_measurements_[0];
+            float phi = measurement_pack.raw_measurements_[1];
+
+            p_x = rho * cos(phi);
+            p_y = rho * sin(phi);
+        }
+    
+        else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+            p_x = measurement_pack.raw_measurements_[0];
+            p_y = measurement_pack.raw_measurements_[1];
+        }
+
+    	ekf_.x_ << p_x, p_y, v, yaw_rate, yaw_rate_dot;
+
+	}
+
+	is_initialized_ = true;
+	return; 
+
+	/*****************************************************************************
+	*  Prediction
+	****************************************************************************/
+
+	float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+	previous_timestamp_ = measurement_pack.timestamp_;
+
+	// Predict sigma points
+	for (int i = 0; i< 2*n_aug+1; i++){
+
+		// Extract values for better readability
+		double p_x = Xsig_aug(0,i);
+		double p_y = Xsig_aug(1,i);
+		double v = Xsig_aug(2,i);
+		double yaw_rate = Xsig_aug(3,i);
+		double yaw_rate_dot = Xsig_aug(4,i);
+		double nu_a = Xsig_aug(5,i);
+		double nu_yawdd = Xsig_aug(6,i);
+
+		// Predicted state values
+		double px_p, py_p;
+
+		// Avoid division by zero
+		if (fabs(yaw_rate_dot) > 0.001) {
+			px_p = p_x + v/yaw_rate_dot * (sin(yaw_rate + yaw_rate_dot * dt) - sin(yaw_rate));
+			py_p = p_y + v/yaw_rate_dot * (cos(yaw_rate) - cos(yaw_rate + yaw_rate_dot * dt));
+		} else {
+			px_p = p_x + v * dt * cos(yaw_rate);
+			py_p = p_y + v * dt * sin(yaw_rate);
+		}
+	}
+
+	double v_p = v;
+	double yaw_p = yaw_rate + yaw_rate_dot * dt ;
+	double yawd_p = yaw_rate_dot;
+
+	// Add noise
+	px_p = px_p + 0.5 * nu_a * dt * dt  * cos(yaw_rate);
+	py_p = py_p + 0.5 * nu_a * dt * dt  * sin(yaw_rate);
+	v_p = v_p + nu_a * dt ;
+
+	yaw_p = yaw_p + 0.5 * nu_yawdd * dt * dt ;
+	yawd_p = yawd_p + nu_yawdd * dt ;
+
+	// Write predicted sigma point into right column
+	Xsig_pred(0,i) = px_p;
+	Xsig_pred(1,i) = py_p;
+	Xsig_pred(2,i) = v_p;
+	Xsig_pred(3,i) = yaw_p;
+	Xsig_pred(4,i) = yawd_p;
+
+
+	/*****************************************************************************
+	*  Update
+	****************************************************************************/
 	if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
 
 	}
