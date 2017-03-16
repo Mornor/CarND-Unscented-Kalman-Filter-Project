fb422dcf3a0a4e7eb356d9cb5d8af70ae0c92b41
Celien Nanson <cesliens@gmail.com>
2017-03-16 20:55:33 +0100

    Update Lidar

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -177,6 +177,21 @@ void UKF::Prediction(double dt) {
  */
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
 	// TODO: Calulate the NIS
+	VectorXd z = measurement_pack.raw_measurements_;
+
+    VectorXd z_pred = H_laser_ * x_;
+    VectorXd z_diff = z - z_pred;
+    MatrixXd Ht = H_laser_.transpose();
+    MatrixXd S = H_laser_ * P_ * Ht + R_laser_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * z_diff);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_laser_) * P_;
 }
 
 /**
