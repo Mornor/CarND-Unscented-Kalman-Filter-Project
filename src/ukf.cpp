f3d1ec96e9b87eded375a9df58e0b5c5101ce868
Celien Nanson <cesliens@gmail.com>
2017-03-21 19:45:40 +0100

    Typo

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -89,10 +89,10 @@ UKF::UKF() {
 
 	// Weights vector
   	VectorXd weights = VectorXd(2 * n_aug_ +  1);
-  	double weight_0 = lambda/(lambda+n_aug);
+  	double weight_0 = lambda / (lambda + n_aug_);
   	weights(0) = weight_0;
-  	for (int i=1; i<2*n_aug+1; i++) {  //2n+1 weights
-    	double weight = 0.5/(n_aug+lambda);
+  	for (int i = 1; i < 2 * n_aug_ + 1; i++) {  //2n+1 weights
+    	double weight = 0.5 / (n_aug_ + lambda);
     	weights(i) = weight;
   	}
 
