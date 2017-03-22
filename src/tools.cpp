91bee4fe4d0735e701d707900ded0d104c9d7c9a
Celien Nanson <cesliens@gmail.com>
2017-03-22 14:06:19 +0100

    Adapt RMSE method

--- src/tools.cpp
+++ src/tools.cpp
@@ -6,33 +6,45 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth) {
-	VectorXd rmse(4);
+		VectorXd rmse(4);
+	vector<VectorXd> estm;
 	rmse << 0,0,0,0;
 
-	// check the validity of the following inputs:
-	//  * the estimation vector size should not be zero
-	//  * the estimation vector size should equal ground truth vector size
-	if(estimations.size() != ground_truth.size() || estimations.size() == 0){
-		cout << "Invalid estimation or ground_truth data" << endl;
+	// check for empty estimations
+	if (estimations.size() == 0) {
+		cout << "CalculateRMSE ERROR: estimation vector size should not be zero" << endl;
 		return rmse;
 	}
 
-	//accumulate squared residuals
-	for(unsigned int i=0; i < estimations.size(); ++i){
-
-		VectorXd residual = estimations[i] - ground_truth[i];
-
-		//coefficient-wise multiplication
-		residual = residual.array()*residual.array();
-		rmse += residual;
+	// check for estimations and ground_truth vectors being equal size.
+	if (estimations.size() != ground_truth.size()) {
+		cout << "CalculateRMSE ERROR: estimation vector size should equal ground_truth size" << endl;
+		return rmse;
 	}
 
-	//calculate the mean
-	rmse = rmse/estimations.size();
+	for (int i=0; i < estimations.size(); ++i) {
+		VectorXd converted(4);
+		converted << estimations[i][0],						// px
+				 estimations[i][1],							// py
+				 cos(estimations[i][3])*estimations[i][2],	// vx
+				 sin(estimations[i][3])*estimations[i][2];	// vy
+		estm.push_back(converted);
+	}
 
-	//calculate the squared root
+	//accumulate squared residuals errors
+	for (int i=0; i < estm.size(); ++i) {
+		VectorXd errors = estm[i] - ground_truth[i];
+		errors = errors.array() * errors.array();
+		rmse += errors;
+	}
+
+	//calculate the mean from the rolling sum
+	rmse /= estm.size();
+
+	//calculate the squared root from the mean
 	rmse = rmse.array().sqrt();
 
-	//return the result
+	//return the RMSE for this estimation.
 	return rmse;
+}
 }
