1b0b7e40590138d522b6a448515b9d10ba88d00a
Celien Nanson <cesliens@gmail.com>
2017-04-04 20:28:45 +0200

    Better RMSE by fixing the process noise standard deviation

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -33,7 +33,7 @@ UKF::UKF() {
 	std_a_ = 3;
 
 	// Process noise standard deviation yaw acceleration in rad/s^2
-	std_yawdd_ = 0.09;
+	std_yawdd_ = 0.9;
 
 	// Laser measurement noise standard deviation position1 in m
 	std_laspx_ = 0.15;
